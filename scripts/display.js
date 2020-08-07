5064e127bb40824ea2ea26ff6478658a962df7f4
losslessring <45686453+losslessring@users.noreply.github.com>
2020-08-08 02:27:53 +0300

    Add files via upload

--- scripts/display.js
+++ scripts/display.js
@@ -6,8 +6,8 @@ const displayField = (container, rows, cols) => {
     container.style.setProperty('--grid-cols', cols);
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild(cell).className = "grid-item";
+        cell.innerText = (c);
+        container.appendChild(cell).className = "cell";
     }
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 import Field from './field.js'
 import {displayField} from './display.js'
+import {getRandomInt} from './utility.js'
 
 
+const rows = 10
+const cols = 5
 
+displayField(document.getElementById("container"), rows, cols)
 
-displayField(document.getElementById("container"), 20, 10)
+// let timerId = setInterval(() => {
+
+// 			document.getElementsByClassName('cell')[getRandomInt(0, rows * cols)].style.backgroundColor = "blue"
+// 						}, 500)
