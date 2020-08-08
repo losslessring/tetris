e696f5e86344eaf33424c08a0c9c06e4f71e5b43
losslessring <45686453+losslessring@users.noreply.github.com>
2020-08-08 03:17:45 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ import {getRandomInt} from './utility.js'
 
 
 const rows = 10
-const cols = 5
+const cols = 10
 
 displayField(document.getElementById("container"), rows, cols)
 
@@ -12,3 +12,22 @@ displayField(document.getElementById("container"), rows, cols)
 
 // 			document.getElementsByClassName('cell')[getRandomInt(0, rows * cols)].style.backgroundColor = "blue"
 // 						}, 500)
+
+// const fall = (cell, increment) => {
+
+// 					setInterval(() => {
+// 						document.getElementsByClassName('cell')[cell].style.backgroundColor = "blue"
+// 						cell = cell + increment
+
+// 						}, 500)
+// 		}
+
+// fall(1, 10)
+
+// Генерация элемента - случайная позиция в верхнем ряду - от 0 до rows
+// Сделать падение элемента - следующая клетка внизу - это номер начального элемента + количество рядов
+//Как сделать это в цикле ? Как передавать следующее значение в интервал или цикл с задержкой ?
+// Можно сделать через замыкание - сделать внешний счетчик и обернуть в функцию
+//Да, все аргументы, переданные в функцию, попадают в ее внутренний объект с аргументами,
+// и этот объект можно менять во внутренней функции
+// Можно сделать через внешний счетчик по ссылке - сделать объект и менять его поле
